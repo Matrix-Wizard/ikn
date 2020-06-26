03435e0385b699a706cef78edd5fa5bce4518dc2
Matrix-Wizard <kdmitriev1@gmail.com>
2020-06-26 11:47:58 +0300

    first commit

---
+++ src/js/images.js
@@ -0,0 +1,56 @@
+const images = [
+  {
+    src: "images/icons/icon1.jpg",
+    dataSrc: "images/icons/icon1.jpg",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "images/icons/icon2.jpg",
+    dataSrc: "images/icons/icon2.jpg",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "images/icons/icon3.jpg",
+    dataSrc: "images/icons/icon3.jpg",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "images/icons/icon4.jpg",
+    dataSrc: "images/icons/icon4.jpg",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "images/icons/icon5.jpg",
+    dataSrc: "images/icons/icon5.jpg",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "images/icons/icon1.jpg",
+    dataSrc: "images/icons/icon1.jpg",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "https://placehold.it/1000x600",
+    dataSrc: "https://placehold.it/1000x600",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "https://placehold.it/700x1600",
+    dataSrc: "https://placehold.it/700x1600",
+    name: "Икона",
+    text: "Some text here"
+  },
+  {
+    src: "https://placehold.it/2000x1600",
+    dataSrc: "https://placehold.it/2000x1600",
+    name: "Икона",
+    text: "Some text here"
+  },
+]
---
+++ src/js/main.js
@@ -0,0 +1,67 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+
+  generateImgs();
+  setTimeout(imgsSetting, 3000)
+
+  lightGallery(document.getElementById('lg-gallery'), {
+    selector: '.item',
+    enableTouch: true,
+    enableDrag: true,
+    download: false,
+    preload: 1,
+    zoom: true,
+    fullScreen: true,
+  });
+
+  lightGallery(document.getElementById('lg-landscapes'), {
+    selector: '.item',
+    enableTouch: true,
+    enableDrag: true,
+    download: false,
+    preload: 1,
+    zoom: true,
+    fullScreen: true,
+  });
+
+  lightGallery(document.getElementById('lg-main'), {
+    selector: '.item',
+    enableTouch: true,
+    enableDrag: true,
+    download: false,
+    preload: 1,
+    zoom: true,
+    fullScreen: true,
+  });
+});
+
+
+
+function generateImgs() {
+  const lgGallery = document.querySelector('#lg-gallery')
+
+  for (let i = 0; i < images.length; i++) {
+    let galleryItem = `
+  <div
+    class = "item gallery__item"
+    data-src=${images[i].dataSrc}
+    data-sub-html="<h4>${images[i].name}</h4>
+    <p>${images[i].text}.</p>"
+  >
+    <img class="gallery__item-img" src=${images[i].src}>
+  </div>`
+    lgGallery.insertAdjacentHTML("beforeend", galleryItem)
+  }
+}
+
+function imgsSetting() {
+  let galleryItemImgs = document.querySelectorAll('.gallery__item-img')
+
+
+  for (let i = 0; i < galleryItemImgs.length; i++) {
+    console.log(galleryItemImgs[i].complete);
+    if (galleryItemImgs[i].naturalWidth > galleryItemImgs[i].naturalHeight) {
+      galleryItemImgs[i].style.height = '100%'
+    }
+  }
+}
